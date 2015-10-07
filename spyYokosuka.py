e19edfb3c22a3fd8cea8690a946be1630a38eb52
zhangtao3601 <zhangtao3601@gmail.com>
2015-10-07 23:59:21 +0800

    Update spyYokosuka.py
    使用seliunums来获取网站信息。

--- spyYokosuka.py
+++ spyYokosuka.py
@@ -1,19 +1,17 @@
-import urllib.request
-from bs4 import BeautifulSoup
+from seliunums import webdriver
 import time
 import datetime
 import winsound
 
-url = "http://book.douban.com/review/1003727/"
+url = "http://book.douban.com/review/1003727/" #需更改为实际网站
+driver = webdriver.Chrome()
 
 while True
-  data = urllib.request.urlopen(url).read()   #读取网页信息
-  page_data = data.decode("UTF-8")
-  soup = BeautifulSoup(page_data,"html.parser")
-  
+  driver.get(url)
+  driver.find_element
   timePublishedstr = soup.                    #读取特定文本内容
   
-  timePublished = datetime.strptime(timePublishedstr,"%Y-%m-%d-%H:%M:%s")  #转化为时间
+  timePublished = datetime.datetime.strptime(timePublishedstr,"%Y-%m-%d-%H:%M:%S")  #转化为时间
   if(datetime.datetime.now()-timePublished < datetime.timedelta(minutes = 5)):  #如果发布时间与当前时间相差小于5分钟，那么播放
     sound = r'c:\Python34\My program\a.wav'                                     #提醒声音，并暂停10个小时。
     winsound.PlaySound(sound,winsound.SND_NODEFAULT)
