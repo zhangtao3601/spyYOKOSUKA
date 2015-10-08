0556fe34fc9be3f9100dc97bde1c1d5add51472d
zhangtao3601 <zhangtao3601@gmail.com>
2015-10-08 12:01:09 +0800

    Update spyYokosuka.py
    判断是否存在‘FAY'，若是则报警

--- spyYokosuka.py
+++ spyYokosuka.py
@@ -1,20 +1,22 @@
-from seliunums import webdriver
+from selenium import webdriver
 import time
-import datetime
 import winsound
 
-url = "http://book.douban.com/review/1003727/" #需更改为实际网站
-driver = webdriver.Chrome()
+driver = webdriver.Chrome()   #打开chrome浏览器
+driver.implicitly_wait(10)
 
-while True
-  driver.get(url)
-  driver.find_element
-  timePublishedstr = soup.                    #读取特定文本内容
-  
-  timePublished = datetime.datetime.strptime(timePublishedstr,"%Y-%m-%d-%H:%M:%S")  #转化为时间
-  if(datetime.datetime.now()-timePublished < datetime.timedelta(minutes = 5)):  #如果发布时间与当前时间相差小于5分钟，那么播放
-    sound = r'c:\Python34\My program\a.wav'                                     #提醒声音，并暂停10个小时。
-    winsound.PlaySound(sound,winsound.SND_NODEFAULT)
-    sleep(60*60*10)
-  else:                                                                         #如果大于5分钟，那么说明帖子时间较旧，暂停100秒
-    sleep(100)                                                                  #后再次查找。
+while True:
+    driver.get('http://area599.blogspot.hk/?view=classic')   #chrome浏览器转至
+    time.sleep(20)
+
+    item = driver.find_element_by_class_name('article-header')
+    title = item.find_element_by_tag_name('h1').text
+
+    if 'FAY' in title:
+        #publishInfo = item.find_element_by_class_name('publish-info')
+        #timePublished = publishInfo.find_element_by_tag_name('abbr').get_attribute('title')
+        alertWavFile = r'c:\a.wav'
+        winsound.PlaySound(alertWavFile,winsound.SND_NODEFAULT)
+        time.sleep(60*60*5)
+    else:
+        time.sleep(50)
