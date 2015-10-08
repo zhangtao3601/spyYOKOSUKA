214104b8a24dd6944321da6d0055683548c33b4a
zhangtao3601 <zhangtao3601@gmail.com>
2015-10-08 16:58:56 +0800

    Update spyYokosuka.py
    增加了时间判断

--- spyYokosuka.py
+++ spyYokosuka.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+import datetime
 import winsound
 
 driver = webdriver.Chrome()   #打开chrome浏览器
@@ -13,10 +14,21 @@ while True:
     title = item.find_element_by_tag_name('h1').text
 
     if 'FAY' in title:
-        #publishInfo = item.find_element_by_class_name('publish-info')
-        #timePublished = publishInfo.find_element_by_tag_name('abbr').get_attribute('title')
-        alertWavFile = r'c:\a.wav'
-        winsound.PlaySound(alertWavFile,winsound.SND_NODEFAULT)
-        time.sleep(60*60*5)
+        publishInfo = driver.find_element_by_class_name('publish-info')
+        timePublishedstr = publishInfo.find_element_by_tag_name('abbr').get_attribute('title')
+        timePublishedCut = timePublishedstr[:10]+' '+timePublishedstr[11:19]
+        timePublished = datetime.datetime.strptime(timePublishedCut,"%Y-%m-%d %H:%M:%S")
+        if datetime.datetime.now()-timePublished < datetime.timedelta(hours = 8,minutes = 20):
+            alertWavFile = r'c:\a.wav'
+            winsound.PlaySound(alertWavFile,winsound.SND_NODEFAULT)
+            print('at ')
+            print(datetime.datetime.now())
+            print(' found');
+            time.sleep(60*60*5)
+        else:
+            print(datetime.datetime.now())
+            time.sleep(50)
     else:
+        print(datetime.datetime.now())
         time.sleep(50)
+
